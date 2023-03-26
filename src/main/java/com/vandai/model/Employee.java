f528875b2973796b82791163d5ecea8298cb6684
daik <minichu1103@gmail.com>
2023-03-26 21:24:19 +0700

    fox

--- src/main/java/com/vandai/model/Employee.java
+++ src/main/java/com/vandai/model/Employee.java
@@ -1,12 +1,14 @@
 package com.vandai.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Employee {
     private String firstName;
     private String lastName;
